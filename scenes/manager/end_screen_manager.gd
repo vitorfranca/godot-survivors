e6b61ec8c846280b30528b8c1875e62983515bbd
Vitor Franca <vitufr@gmail.com>
2024-05-25 20:23:57 -0700

    create end screen manager to encapsulate end screen logic

--- scenes/manager/end_screen_manager.gd
+++ scenes/manager/end_screen_manager.gd
@@ -1,12 +1,15 @@
 extends Node
+class_name EndScreenManager
 
 @onready var player = %Player
 
 @export var end_screen_scene: PackedScene
 
+
 func _ready():
+	await player.ready
 	player.health_component.died.connect(on_player_died)
-	
+
 
 func on_player_died():
 	var end_screen_instance = end_screen_scene.instantiate()
