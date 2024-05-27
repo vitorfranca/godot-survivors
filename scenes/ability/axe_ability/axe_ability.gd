d56a5270068c9ff0c91dadb64d145e98b71f7076
Vitor Franca <vitufr@gmail.com>
2024-05-27 09:49:01 -0700

    axe spiraling from the launched position

--- scenes/ability/axe_ability/axe_ability.gd
+++ scenes/ability/axe_ability/axe_ability.gd
@@ -6,21 +6,21 @@ const MAX_RADIUS = 100
 @onready var hitbox_component = $HitboxComponent
 
 var base_rotation = Vector2.RIGHT
-
+var player_initial_position: Vector2 = Vector2.ZERO
 
 func _ready():
 	base_rotation = Vector2.RIGHT.rotated(randf_range(0, TAU))
 	var tween = create_tween()
 	tween.tween_method(tween_method, 0.0, 2.0, 2)
 	tween.tween_callback(queue_free)
-	
-
-func tween_method(rotations: float):
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
-	
+	player_initial_position = player.global_position
+
+
+func tween_method(rotations: float):
 	var percentage = rotations / 2
 	var current_radius = percentage * MAX_RADIUS
 	var current_direction = base_rotation.rotated(rotations * TAU)
-	global_position = player.global_position + (current_direction * current_radius)
+	global_position = player_initial_position + (current_direction * current_radius)
