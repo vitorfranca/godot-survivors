2655e76ec3e3d4b38358808c1991750a6746e350
Vitor Franca <vitufr@gmail.com>
2024-05-27 10:17:29 -0700

    enemy walk animation

---
+++ scenes/game_object/enemy/rat/rat.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed = 35
+@onready var health_component: HealthComponent = $HealthComponent
+
+func _process(_delta):
+	var direction = get_direction_to_player()
+	velocity = direction * speed
+	move_and_slide()
+
+
+func get_direction_to_player():
+	var player_node = get_tree().get_first_node_in_group("player") as Node2D
+	if player_node != null:
+		return (player_node.global_position - global_position).normalized()
+	return Vector2.ZERO
