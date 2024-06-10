21b2af59e26ea5a0595e115dfd54292f4825ad7c
Vitor Franca <vitufr@gmail.com>
2024-06-09 22:42:38 -0700

    [feat] options menu with volume and window mode

---
+++ scenes/UI/options_menu.gd
@@ -0,0 +1,60 @@
+extends CanvasLayer
+
+signal back_pressed
+
+@onready var window_button: Button = %WindowButton
+@onready var sfx_slider = %SFXSlider
+@onready var music_slider = %MusicSlider
+
+
+func _ready():
+	window_button.pressed.connect(on_window_button_pressed)
+	%BackButton.pressed.connect(on_back_button_pressed)
+	sfx_slider.value_changed.connect(on_audio_slider_changed.bind('sfx'))
+	music_slider.value_changed.connect(on_audio_slider_changed.bind('music'))
+	update_display()
+
+
+func update_display():
+	sfx_slider.value = get_bus_volume_percent('sfx')
+	music_slider.value = get_bus_volume_percent('music')
+	
+	var mode = DisplayServer.window_get_mode()
+	match mode:
+		DisplayServer.WINDOW_MODE_FULLSCREEN:
+			window_button.text = "Fullscreen"
+		DisplayServer.WINDOW_MODE_WINDOWED:
+			window_button.text = "Windowed"
+
+
+func get_bus_volume_percent(bus_name: String):
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	var volume_db = AudioServer.get_bus_volume_db(bus_index)
+	var volume_linear = db_to_linear(volume_db)
+	return volume_linear
+
+
+func set_bus_volume_percent(bus_name: String, percent: float):
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	var volume_db = linear_to_db(percent)
+	AudioServer.set_bus_volume_db(bus_index, volume_db)
+
+
+func on_audio_slider_changed(value: float, bus_name: String):
+	set_bus_volume_percent(bus_name, value)
+
+
+func on_window_button_pressed():
+	var mode = DisplayServer.window_get_mode()
+	match mode:
+		DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
+	update_display()
+
+
+func on_back_button_pressed():
+	back_pressed.emit()
+
--- scenes/component/button_click_player_component/button_click_player_component.gd
+++ scenes/component/button_click_player_component/button_click_player_component.gd
@@ -6,16 +6,24 @@ extends Node
 
 func _ready():
 	var parent: Node = get_parent()
-	if parent.button_down:
+	if parent.has_signal("button_down"):
 		parent.button_down.connect(on_button_down)
-	if parent.button_up:
-		parent.button_up.connect(on_button_up) 
+	if parent.has_signal("button_up"):
+		parent.button_up.connect(on_button_up)
+	if parent.has_signal("drag_started"):
+		parent.drag_started.connect(on_button_down)
+	if parent.has_signal("drag_ended"):
+		print("drag_ended")
+		parent.drag_ended.connect(on_drag_ended)
 
 
 func on_button_down():
 	button_down_player.play()
-	
-	
+
+
 func on_button_up():
 	button_up_player.play()
 
+
+func on_drag_ended(_value_changed: bool):
+	on_button_up()
