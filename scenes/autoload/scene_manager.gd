8e836c3111f2f13f793b7cd6872c61188a0a72d7
Vitor Franca <vitufr@gmail.com>
2024-06-11 19:13:01 -0700

    disable transition animation

--- scenes/autoload/scene_manager.gd
+++ scenes/autoload/scene_manager.gd
@@ -1,8 +1,7 @@
 extends Node
 
-var main_scene = preload("res://scenes/main/main.tscn")
 
 func load_main_scene(transition: bool = true):
-	ScreenTransition.transition()
-	await ScreenTransition.transition_half
+	#ScreenTransition.transition()
+	#await ScreenTransition.transition_half
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
